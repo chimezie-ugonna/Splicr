21941b62cdd9eee59e4e1e4547f454ff01c6d2ef
Ugonna Chimezie <ugiezie@gmail.com>
2024-09-26 13:06:46 +0100

    - Fixed issue where purchase result was being recomposed.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.splicr.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 12
-        versionName = "5.5.2"
+        versionCode = 13
+        versionName = "5.5.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
