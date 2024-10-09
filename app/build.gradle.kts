7087d770d5e1f0331e99514fd2b8b33242542d17
Ugonna Chimezie <ugiezie@gmail.com>
2024-10-09 09:10:05 +0100

    - Bug fixes.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.splicr.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 17
-        versionName = "6.0.2"
+        versionCode = 18
+        versionName = "6.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
